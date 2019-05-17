20dbed1ae059cb4d98d95acb336ef7f7d482f4b9
Anthony H <anthonyyh403@gmail.com>
2019-05-16 20:25:51 -0700

    html, css, python file

--- astronauts_api.py
+++ astronauts_api.py
@@ -1,6 +1,17 @@
+#importing modules needed for data visualization
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+%matplotlib inline
+
+# change plotting colors per client request
+plt.style.use('ggplot')
+
+# Increase default figure and font sizes for easier viewing.
+plt.rcParams['figure.figsize'] = (8, 6)
+plt.rcParams['font.size'] = 14
 
 astro = pd.read_csv('/Users/anthony/Desktop/flask_app/astronaut-yearbook.zip')
 
